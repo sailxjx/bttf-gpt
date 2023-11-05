432e8e471d4d5e11a46a0a7d38d44e18fcc4d202
Xu Jingxin <sailxjx@gmail.com>
2023-11-05 17:21:22 +0800

    Init play page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,36 @@
+'use client'
+
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import './home.css'
 
 export default function Home() {
+  const router = useRouter()
+  const [animate, setAnimate] = useState(false)
+
+  const handleClick = () => {
+    setAnimate(true)
+    setTimeout(() => {
+      router.push('/play')
+    }, 1000) // Replace 2000 with the duration of your animation in milliseconds
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-40">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-20">
       <div className="z-10 max-w-5xl w-full flex items-center justify-center font-mono text-sm lg:flex">
+        <h1 className='text-white text-4xl'>Back to the Future as Interpreted by ChatGPT</h1>
+      </div>
+      <div className={`z-10 max-w-5xl w-full flex items-center justify-center font-mono text-sm lg:flex ${animate ? 'animate-car' : ''}`}>
         <Image src="/images/dmc12.webp" alt="DMC-12" width={480} height={320} />
       </div>
       <div className="z-10 max-w-5xl w-full flex items-center justify-center font-mono text-sm lg:flex">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>
           Start time traveling
         </button>
       </div>
-      <Image src="/images/city.webp" alt="City background" layout='fill' objectFit='cover' className="absolute z-0" />
+      <Image src="/images/city.webp" alt="City background" width={0}
+        height={0} sizes="100vw" className="absolute z-0 bg-img" />
     </main>
   )
 }
---
+++ app/play/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-40">
+      <h1>Time traveling</h1>
+    </main>
+  )
+}
